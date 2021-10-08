993fb502d661fe7b6bbaa4bb8fc9e8836578fe5c
willuwontu <willuwontu3@gmail.com>
2021-10-08 11:39:17 -0700

    Work on Assets

--- RebindControls/RebindControls.cs
+++ RebindControls/RebindControls.cs
@@ -1,9 +1,16 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using UnboundLib;
-using UnboundLib.Utils;
+using UnboundLib.Utils.UI;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Jotunn.Utils;
+using UnityEngine.UI;
 using InControl;
+using System;
+using TMPro;
 
 namespace RebindControls
 {
@@ -21,9 +28,15 @@ namespace RebindControls
         private const string ModName = "Rebind Controls";
         public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
 
-        private PlayerActions keyboardPlayer = new PlayerActions();
-
-        private
+        public static RebindControls instance { get; private set; }
+        private PlayerActions keyboardPlayer;
+        private PlayerActions controllerPlayer;
+        private ConfigEntry<string> keyboardBindingsConfig;
+        private ConfigEntry<string> controllerBindingsConfig;
+        internal AssetBundle UIAssets;
+        private GameObject containerFrameAsset;
+        private GameObject keyGroupAsset;
+        private GameObject bindingButtonAsset;
 
         void Awake()
         {
@@ -33,7 +46,166 @@ namespace RebindControls
         }
         void Start()
         {
-            Unbound.RegisterCredits(ModName, new string[] { "willuwontu" }, new string[] { "github", "Ko-Fi" }, new string[] { "https://github.com/willuwontu/wills-wacky-cards", "https://ko-fi.com/willuwontu" });
+            instance = this;
+
+            UnityEngine.Debug.Log($"[RebindControls] Registering Credits");
+            Unbound.RegisterCredits(ModName, new string[] { "willuwontu" }, new string[] { "github", "Ko-Fi" }, new string[] { "https://github.com/willuwontu/RebindControls", "https://ko-fi.com/willuwontu" });
+
+            UnityEngine.Debug.Log($"[RebindControls] Initializing Default Controls");
+            InitializeDefaultControls();
+
+            UnityEngine.Debug.Log($"[RebindControls] Setting up config binds.");
+            //Setup the BepInEx config binds
+            {
+                keyboardBindingsConfig = Config.Bind("Controls", "Keyboard", keyboardPlayer.Save(), "The control layout for a keyboard and mouse user.");
+                controllerBindingsConfig = Config.Bind("Controls", "Controller", controllerPlayer.Save(), "The control layout for a controller user.");
+                keyboardPlayer.Load(keyboardBindingsConfig.Value);
+                controllerPlayer.Load(controllerBindingsConfig.Value);
+            }
+
+            UnityEngine.Debug.Log($"[RebindControls] Initializing loading of assets.");
+            LoadAssets();
+
+            UnityEngine.Debug.Log($"[RebindControls] Registering Menu");
+            Unbound.RegisterMenu(ModName, () => { }, this.MainGui, null, true);
+
+            UnityEngine.Debug.Log($"[RebindControls] Adding network events.");
+            var networkEvents = gameObject.AddComponent<NetworkEventCallbacks>();
+            networkEvents.OnJoinedRoomEvent += OnJoinedRoomAction;
+            networkEvents.OnLeftRoomEvent += OnLeftRoomAction;
+        }
+
+        private void InitializeDefaultControls()
+        {
+            keyboardPlayer = PlayerActions.CreateWithKeyboardBindings();
+            controllerPlayer = PlayerActions.CreateWithControllerBindings();
+            // Setup the default profile for a keyboard player
+            {
+                keyboardPlayer.ListenOptions.IncludeControllers = false;
+                keyboardPlayer.ListenOptions.IncludeNonStandardControls = false;
+                keyboardPlayer.ListenOptions.IncludeUnknownControllers = false;
+                keyboardPlayer.ListenOptions.MaxAllowedBindings = 1U;
+                keyboardPlayer.ListenOptions.UnsetDuplicateBindingsOnSet = true;
+                keyboardPlayer.ListenOptions.IncludeMouseButtons = true;
+                keyboardPlayer.ListenOptions.OnBindingFound += OnBindingFound;
+                keyboardPlayer.ListenOptions.OnBindingRejected += OnBindingRejectedMenu;
+                keyboardPlayer.ListenOptions.OnBindingAdded += OnBindingAdded;
+            }
+
+            // Setup the default profile for a controller player
+            {
+                controllerPlayer.ListenOptions.IncludeUnknownControllers = true;
+                controllerPlayer.ListenOptions.MaxAllowedBindings = 3U;
+                controllerPlayer.ListenOptions.UnsetDuplicateBindingsOnSet = true;
+                controllerPlayer.ListenOptions.OnBindingFound += OnBindingFoundController;
+                controllerPlayer.ListenOptions.OnBindingRejected += OnBindingRejectedMenu;
+                controllerPlayer.ListenOptions.OnBindingAdded += OnBindingAdded;
+            }
+        }
+
+        private void LoadAssets()
+        {
+            UnityEngine.Debug.Log($"[RebindControls] Attempting to load assetbundle.");
+            UIAssets = AssetUtils.LoadAssetBundleFromResources("rebindcontrolsui", typeof(RebindControls).Assembly);
+
+            foreach (var asset in UIAssets.GetAllAssetNames())
+            {
+                UnityEngine.Debug.Log($"[RebindControls][UI Asset] {asset}");
+            }
+
+            UnityEngine.Debug.Log($"[RebindControls] Attempting to load assets.");
+            containerFrameAsset = UIAssets.LoadAsset<GameObject>("BindingGridFrame");
+            keyGroupAsset = UIAssets.LoadAsset<GameObject>("KeyGroup");
+            bindingButtonAsset = UIAssets.LoadAsset<GameObject>("Binding");
+        }
+
+        private void OnJoinedRoomAction()
+        {
+
+        }
+
+        private void OnLeftRoomAction()
+        {
+
+        }
+
+        private void OnBindingAdded(PlayerAction action, BindingSource binding)
+        {
+            Debug.Log("Binding added... " + binding.DeviceName + ": " + binding.Name);
+        }
+
+        private bool OnBindingFound(PlayerAction action, BindingSource binding)
+        {
+            if (binding == new KeyBindingSource(Key.Escape))
+            {
+                action.StopListeningForBinding();
+                return false;
+            }
+            return true;
+        }
+
+        private bool OnBindingFoundController(PlayerAction action, BindingSource binding)
+        {
+            if (binding.BindingSourceType != BindingSourceType.DeviceBindingSource)
+            {
+                action.StopListeningForBinding();
+                return false;
+            }
+            return true;
+        }
+
+        private void OnBindingRejectedMenu(PlayerAction action, BindingSource binding, BindingSourceRejectionType reason)
+        {
+            Debug.Log("Binding rejected... " + reason);
+        }
+
+        private void MainGui(GameObject menu)
+        {
+            MenuHandler.CreateText(ModName + " Options", menu, out TextMeshProUGUI _, 60);
+
+            CreateKeyBindingLayout(menu, keyboardPlayer);
+
+        }
+
+        private void KeyboardGUI(GameObject menu)
+        {
+            MenuHandler.CreateText(ModName + " Options", menu, out TextMeshProUGUI _, 60);
+
+            foreach (var action in keyboardPlayer.Actions)
+            {
+                //MenuHandler.CreateText
+                MenuHandler.CreateButton("", menu);
+            }
+        }
+
+        private GameObject CreateKeyBindingLayout(GameObject parent, PlayerActions bindings)
+        {
+            if (parent.transform.Find("Group/Grid/Scroll View/Viewport/Content"))
+            {
+                parent = parent.transform.Find("Group/Grid/Scroll View/Viewport/Content").gameObject;
+            }
+
+            var container = GameObject.Instantiate(containerFrameAsset, parent.transform);
+
+            return container;
+        }
+    }
+
+    public class RightClick : MonoBehaviour, IPointerClickHandler
+    {
+
+        public UnityEvent leftClick;
+        public UnityEvent middleClick;
+        public UnityEvent rightClick;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left)
+                leftClick.Invoke();
+            else if (eventData.button == PointerEventData.InputButton.Middle)
+                middleClick.Invoke();
+            else if (eventData.button == PointerEventData.InputButton.Right)
+                rightClick.Invoke();
         }
     }
 }
