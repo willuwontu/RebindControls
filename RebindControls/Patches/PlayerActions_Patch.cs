de4cae4ad28cd44f9d7a94478e679f892eeaa28e
willuwontu <willuwontu3@gmail.com>
2021-10-10 00:17:20 -0700

    Updates player controls out of game.

---
+++ RebindControls/Patches/PlayerActions_Patch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace RebindControls.Patches
+{
+    [HarmonyPatch(typeof(PlayerActions))]
+    class PlayerActions_Patch
+    {
+        //[HarmonyPriority(Priority.Last)]
+        [HarmonyPrefix]
+        [HarmonyPatch("CreateWithKeyboardBindings")]
+        static bool KeyboardLayout(ref PlayerActions __result)
+        {
+            if (RebindControls.instance.setupDefault)
+            {
+                UnityEngine.Debug.Log("Default Keyboard layout has been setup already, attempting to substitute the player's layout instead.");
+                __result = RebindControls.instance.keyboardPlayer;
+                return false;
+            }
+            return true;
+        }
+
+        //[HarmonyPriority(Priority.Last)]
+        [HarmonyPrefix]
+        [HarmonyPatch("CreateWithControllerBindings")]
+        static bool ControllerLayout(ref PlayerActions __result)
+        {
+            if (RebindControls.instance.setupDefault)
+            {
+                UnityEngine.Debug.Log("Default Controller layout has been setup already, attempting to substitute the player's layout instead.");
+                __result = RebindControls.instance.controllerPlayer;
+                return false;
+            }
+            return true;
+        }
+
+
+        //[HarmonyPrefix]
+        //[HarmonyPatch("SomeMethod")]
+        //static void MyMethodName()
+        //{
+
+        //}
+
+        //[HarmonyPostfix]
+        //[HarmonyPatch("SomeMethod")]
+        //static void MyMethodName()
+        //{
+
+        //}
+    }
+}
